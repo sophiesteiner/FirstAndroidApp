0dd1317292cde5cf190f7a1c3a467c9555db8269
sophiesteiner <sophiest@umich.edu>
2018-09-27 20:51:43 -0400

    init commit pt. 2

--- app/src/main/java/com/example/machine/firstandroidapp/MainActivity.java
+++ app/src/main/java/com/example/machine/firstandroidapp/MainActivity.java
@@ -9,5 +9,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //sample comment, check to ensure commit & push is working
     }
 }
